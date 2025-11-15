01209b9b5620f00b36e644e195ec754a0680792c
sandisundawa <sundawasandi@gmail.com>
2025-11-15 11:20:04 +0700

    run fetch on dispatcher.IO

--- app/src/main/java/id/codeid/pokemon/data/repository/PokeRepositoryImpl.kt
+++ app/src/main/java/id/codeid/pokemon/data/repository/PokeRepositoryImpl.kt
@@ -13,42 +13,48 @@ import kotlinx.coroutines.withContext
 
 class PokeRepositoryImpl(private val api: PokeApi, private val dao: PokemonDao) : PokeRepository {
 
-    override suspend fun fetchPokemonList(limit: Int, offset: Int): List<Pokemon> {
-        val localData = dao.getPokemonList(limit, offset)
+    override suspend fun fetchPokemonList(limit: Int, offset: Int): List<Pokemon> =
+        withContext(Dispatchers.IO) {
 
-        if (localData.isNotEmpty()) {
-            return localData.map {
-                Pokemon(id = it.id, name = it.name)
+            val localData = dao.getPokemonList(limit, offset)
+            if (localData.isNotEmpty()) {
+                return@withContext localData.map {
+                    Pokemon(id = it.id, name = it.name)
+                }
             }
+
+            val resp = api.getPokemonList(limit, offset)
+            val entities = resp.results.mapNotNull { dto ->
+                val id = dto.url
+                    .trimEnd('/')
+                    .split("/")
+                    .lastOrNull()
+                    ?.toIntOrNull()
+
+                id?.let { PokemonEntity(id = id, name = dto.name) }
+            }
+            dao.insertAll(entities)
+            return@withContext entities.map { Pokemon(it.id, it.name) }
         }
 
-        val resp = api.getPokemonList(limit, offset)
 
-        val entities = resp.results.mapNotNull { dto ->
-            val id = dto.url.trimEnd('/').split("/").lastOrNull()?.toIntOrNull()
-            // ambil id nya dari path terakhir dari url
-            // bismillah gak ada yg sama
-            id?.let { PokemonEntity(id = id, name = dto.name) }
+    override suspend fun fetchPokemonDetail(name: String): PokemonDetailResponse =
+        withContext(Dispatchers.IO) {
+            val gson = Gson()
+
+            val local = dao.getDetail(name)
+            if (local != null) {
+                return@withContext gson.fromJson(local.json, PokemonDetailResponse::class.java)
+            }
+
+            val remote = api.getDetailPokemon(name)
+
+            val json = gson.toJson(remote)
+            dao.insert(PokemonDetailEntity(name = name, json = json))
+
+            return@withContext remote
         }
 
-        dao.insertAll(entities)
-
-        return entities.map { Pokemon(it.id, it.name) }
-    }
-
-    override suspend fun fetchPokemonDetail(name: String): PokemonDetailResponse {
-        val gson = Gson()
-        val local = dao.getDetail(name)
-        if (local != null) {
-            return gson.fromJson(local.json, PokemonDetailResponse::class.java)
-        }
-        val remote = api.getDetailPokemon(name)
-
-        val json = gson.toJson(remote)
-        dao.insert(PokemonDetailEntity(name = name, json = json))
-
-        return remote
-    }
 
     override suspend fun searchPokemonLocal(query: String): List<PokemonEntity> =
         dao.searchByName("%$query%")
